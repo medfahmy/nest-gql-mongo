1425caf1121d5bf1a9f0e7f1d77dca5d2a8ae879
medfahmy <medfahmy99@gmail.com>
2021-07-09 17:59:04 +0100

    users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,18 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { GraphQLModule } from "@nestjs/graphql";
-import { CatsModule } from "./cats/cats.module";
+import { UserModule } from "./user/user.module";
 import { MongooseModule } from "@nestjs/mongoose";
+
+import mongoURI from "mongoURI";
 
 @Module({
   imports: [
-    CatsModule,
+    UserModule,
     GraphQLModule.forRoot({
       autoSchemaFile: "schema.gql",
     }),
-    MongooseModule.forRoot("mongodb://localhost/nest"),
+    MongooseModule.forRoot(mongoURI),
   ],
   controllers: [AppController],
   providers: [AppService],
